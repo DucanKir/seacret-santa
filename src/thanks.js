29e14cd1740a8caa4ddd3d62eacee7dfed56fa85
Lana Kirilenko <svetlana.kirilenko@hotmail.com>
2019-11-12 17:39:55 +0000

    thanks

---
+++ src/thanks.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class Thanks extends React.Component {
+
+  render(){
+
+    return (
+      <div className="thanks">
+        <div className="sect">
+          <h1 className="title is-2 has-text-centered has-text-light">Спасибо за регистрацию!</h1>
+          <br />
+          <h1 className="subtitle is-4 has-text-centered has-text-light">Как только все участники зарегистрируют свои анкеты, на указанный вами адрес электронной почты вы получите адрес своего подопечного, для которого станете Анонимным Дедом Морозом!</h1>
+          <h1 className="subtitle is-4 has-text-centered has-text-light">Удачи!</h1>
+        </div>
+      </div>
+    )
+
+  }
+
+}
+
+export default Thanks
